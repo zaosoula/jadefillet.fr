6814499a08eb49d6d7d01e1a5561c9dc1c031695
Zao Soula <contact@zaosoula.fr>
2021-05-26 16:25:31 +0200

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const port = process.env.PORT || 5000;
+(require('dotenv')).config();
+const port = process.env.PORT;
 const express = require("express")
 const app = express()
 const eta = require("eta")
